ad575a2d3b89cfbf11af73d71ca02fd306bc5283
Arpan-Maity032 <arpanmaity032@gmail.com>
2025-07-23 17:59:24 +0530

    Skills section added

--- src/components/Siders.jsx
+++ src/components/Siders.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../styles/sider.css";
 import Arpan_cv from "../media/Arpan_maity_resume2.pdf";
+import { Typography } from "antd";
+import { Tag } from "antd";
 
 import { Button, Layout} from "antd";
 import Sider from 'antd/es/layout/Sider';
@@ -13,6 +15,8 @@ const Siders = () => {
     link.download = "Arpan_Cv.pdf";
     link.click();
   }
+  const skills = [" C++","Java", "Python", "JavaScript", "SQL","HTML5","CSS3","React","Django","Flask","Streamlit","Bootstrap","Express.js",
+"REST APIs","FastAPI"];
   return (
     <Layout>
         <Sider className='sider'>
@@ -27,7 +31,7 @@ const Siders = () => {
               <Button onClick={downloadCv}>Download Resume</Button>
             </div>
         </Sider>
-        <Content className=''>
+        <Content>
           <div className='welcome'>Welcome to my Profile<br/>
           I am a <div class="dropping-texts">
           <div>Developers</div>
@@ -35,6 +39,20 @@ const Siders = () => {
           <div>Coders</div>
           <div>EVERYONE!</div>
         </div></div>
+        <Typography className='objective'>
+          <h1>Objective</h1>
+          <h5>Highly motivated Computer Science Engineering student with a strong foundation in artificial intelligence, machine learning,
+ and software development. <strong>Skilled in Python, c++, Java, SQL, and AWS.</strong> Seeking to apply technical expertise and problem-solving
+ abilities to contribute to innovative projects in a growth-focused organization.</h5>
+        </Typography>
+        <Typography>
+          <h1>Programming SKills/Knowledge:</h1>
+          {skills.map((skill)=>(
+            <Tag color="blue" key={skill}>
+              {skill}
+            </Tag>
+          ))}
+        </Typography>
           </Content>
     </Layout>
   )
