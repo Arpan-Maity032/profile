a24a09efbc5a2c46854d454292d1f3ed0581e15d
Arpan-Maity032 <arpanmaity032@gmail.com>
2025-07-23 07:48:39 +0530

    Sidebar completed

--- src/components/Siders.jsx
+++ src/components/Siders.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import "../styles/sider.css";
+import Arpan_cv from "../media/Arpan_maity_resume2.pdf";
 
-import { Layout } from "antd";
+import { Button, Layout} from "antd";
 import Sider from 'antd/es/layout/Sider';
 import { Content } from 'antd/es/layout/layout';
-import { Color } from 'antd/es/color-picker';
-
+import { Avatar } from "antd";
 const Siders = () => {
+  const downloadCv =()=>{
+    const link = document.createElement("a");
+    link.href = Arpan_cv;
+    link.download = "Arpan_Cv.pdf";
+    link.click();
+  }
   return (
     <Layout>
         <Sider className='sider'>
-            <p>welcome to sider</p>
+            <Avatar className='picture' src="https://i.pravatar.cc/150?img=3" alt="Profile" />
+            <div className='intro'>
+              <h1>Arpan Maity</h1>
+              <p>Kolkata , West Bengal</p>
+              <p>India  <Avatar className="flag" src="https://clipground.com/images/indian-flag-icon-clipart-6.jpg" alt="flag"/></p><br/>
+              <h2>Fullstack Developer<br/>& <br/>AI Engineer</h2>
+            </div>
+            <div className='resume'>
+              <Button onClick={downloadCv}>Download Resume</Button>
+            </div>
         </Sider>
         <Content><p>This is content space</p></Content>
     </Layout>
